8e4fba3a6022dc069139816a0b0c21238381c25f
Subhin Hwang <107633586+Surihub@users.noreply.github.com>
2024-05-21 08:39:44 +0900

    Merged main into plotify-ver1

---
+++ merge_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# 브랜치 목록
+branches=("plotify-ver1" "plotify-ver3" "plotify-ver4" "plotify-ver5" "plotify-ver6" "plotify-ver7" "plotify-ver8" "plotify-ver9" "plotify-ver10")
+
+# 메인 브랜치에서 변경사항 가져오기
+git checkout main
+git pull origin main
+
+# 각 브랜치로 변경사항 병합
+for branch in "${branches[@]}"
+do
+    echo "Merging main into $branch"
+    git checkout "$branch"
+    git merge main
+    git add .
+    git commit -m "Merged main into $branch"
+    git push origin "$branch"
+done
+
+# 다시 메인 브랜치로 이동
+git checkout main
+
